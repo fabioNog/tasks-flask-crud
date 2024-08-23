9d37a03bde28dc3be49216ec610a3c53824d3420
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-08-23 14:13:03 -0300

    update post

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from models.task import Task
 
 app = Flask(__name__)
@@ -10,8 +10,9 @@ task_id_control = 1
 @app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
-    data = request.get_json
-    print(data)
+    data = request.get_json()
+    """ return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id}) """
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
