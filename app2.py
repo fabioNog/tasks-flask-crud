b8b440ac8d5c20e4bf5b024b2a759c4e4bca25b2
fabioNog <fabio.nogueira.carmo@gmail.com>
2024-08-25 20:19:23 -0300

    Create method get

--- app2.py
+++ app2.py
@@ -11,8 +11,27 @@ task_id_control = 1
 def create_task():
     global task_id_control
     data = request.get_json()
-    """ return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id}) """
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    new_task = Task(
+        id=task_id_control, 
+        title=data['title'], 
+        description=data.get("description", "")
+        )
+    task_id_control += 1
+    tasks.append(new_task)
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+
+    return jsonify(output)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
